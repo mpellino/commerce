996c335065d3c7abc60f81e334ad0c907ce0d17f
mpellino <marcopellino00@gmail.com>
2022-01-13 21:39:58 +0100

    Create Auction, Bid, Comments, Winner models
    Empty.

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class AuctionListing(models.Model):
+    pass
+
+
+class Bid(models.Modelodel):
+    pass
+
+
+class Comments(models.Model):
+    pass
+
+
+class AuctionWinner(models.Model):
+    pass
