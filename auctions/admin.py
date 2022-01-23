ab6530f59959f15b8d706380ad940cf242f7e27b
mpellino <marcopellino00@gmail.com>
2022-01-23 14:18:48 +0100

    Add admin.site.register(Bid).

--- auctions/admin.py
+++ auctions/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
-from .models import User, AuctionListing, Comments
+from .models import User, AuctionListing, Comments, Bid
 
 admin.site.register(User, UserAdmin)
 
 admin.site.register(AuctionListing)
 
 admin.site.register(Comments)
+
+admin.site.register(Bid)
